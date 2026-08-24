dc99ec18bb70e505811450dbb3ead8c1b6944d25
vlado-zlatovic <zlatovicvlado99@gmail.com>
2026-08-24 15:01:49 +0200

    changes 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
         <div className='footer-socials'>
             <p>Socials:</p>
             <a href="https://github.com/vlado-zlatovic" target='_blank'><img src="/socials-icons/github-brands-solid-full.svg" alt="" /></a>
-            <a href=""><img src="/socials-icons/linkedin-brands-solid-full.svg" alt="" /></a>
+            <a href="https://www.linkedin.com/in/vlado-zlatovic-741553416/" target='_blank'><img src="/socials-icons/linkedin-brands-solid-full.svg" alt="" /></a>
         </div>
       </div>
     </footer>
